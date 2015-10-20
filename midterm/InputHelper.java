fe552d6d1011feddd8c7e5d9d5cbec4d4a8f99d5
tiltedlistener <tiltedlistener@gmail.com>
2015-10-19 18:04:15 -0700

    InputHelper

---
+++ midterm/InputHelper.java
@@ -0,0 +1,20 @@
+package Main;
+
+import java.io.*;
+
+public class InputHelper {
+
+	public String getUserInput(String prompt) {
+		String inputLine = null;		
+		System.out.print(prompt + " ");
+		try {
+			BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+			inputLine = is.readLine();
+			if (inputLine.length() == 0) return null;
+		} catch (IOException e) {
+			System.out.println("IOException: " + e);
+		}
+		return inputLine;
+	}
+	
+}
